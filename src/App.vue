8977f2644c0d8d5705f21d8f5b49e80036f0d02b
Maksim_Smetanin <maks.s923@mail.ru>
2019-10-24 22:03:34 +0500

    create basic html markup -> views components and creating margrut for them

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <div id="app">
-    
+  <div id="wrapper">
+    <component :is="isLayout"></component>
   </div>
 </template>
+
+<script>
+import Empty from '@/layouts/EmptyLayout'
+import Main from '@/layouts/MainLayout'
+
+export default {
+  name: 'app',
+
+  components: {
+    Main,
+    Empty
+  },
+
+  computed: {
+    isLayout() {
+      return this.$route.meta.layout || 'Empty'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~materialize-css/sass/materialize.scss';
+@import '@/assets/style.scss';
+</style>
